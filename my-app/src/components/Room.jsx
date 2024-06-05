50df899afb8c2ea30d1c5dca26276c2c784f4c2a
hauntedguest <99336943+hauntedguest@users.noreply.github.com>
2024-06-06 04:03:18 +0530

    Final

--- my-app/src/components/Room.jsx
+++ my-app/src/components/Room.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {useParams  } from "react-router-dom"
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 const Room = () => {
     const {id}=useParams();
     let myMeeting = async (element) => {
         // generate Kit Token
-         const appID = ;
-         const serverSecret = "";
-         const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
+         const appID = 1884139254;
+         const serverSecret = "b509e2481652cbc77d4afbaa0b7bd606";
+         const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID,serverSecret,id,Date.now().toString(),"Pranav");
    
        
         // Create instance object from Kit Token.
@@ -17,21 +18,18 @@ const Room = () => {
            sharedLinks: [
              {
                name: 'Personal link',
-               url:
-                window.location.protocol + '//' + 
-                window.location.host + window.location.pathname +
-                 '?roomID=' +
-                 roomID,
+               url:`http://localhost:5173/room/${id}`
              },
            ],
            scenario: {
-             mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+             mode: ZegoUIKitPrebuilt.OneONoneCall , // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
            },
          });
         };
   return (
-    <>
-    </>
+    <div className="myCallContainer" ref={myMeeting}>
+        
+    </div>
   )
 }
 
