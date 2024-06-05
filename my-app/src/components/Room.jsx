98d4ff9f36802bdc6ed560ac84643f9ed7a86b15
hauntedguest <99336943+hauntedguest@users.noreply.github.com>
2024-06-06 03:45:09 +0530

    commit

--- my-app/src/components/Room.jsx
+++ my-app/src/components/Room.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
-
+import {useParams  } from "react-router-dom"
 const Room = () => {
+    const {id}=useParams();
+    let myMeeting = async (element) => {
+        // generate Kit Token
+         const appID = ;
+         const serverSecret = "";
+         const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
+   
+       
+        // Create instance object from Kit Token.
+         const zp = ZegoUIKitPrebuilt.create(kitToken);
+         // start the call
+         zp.joinRoom({
+           container: element,
+           sharedLinks: [
+             {
+               name: 'Personal link',
+               url:
+                window.location.protocol + '//' + 
+                window.location.host + window.location.pathname +
+                 '?roomID=' +
+                 roomID,
+             },
+           ],
+           scenario: {
+             mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+           },
+         });
+        };
   return (
     <>
     </>
