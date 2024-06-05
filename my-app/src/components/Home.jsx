c83fb4fa0fe3d72998890f09d5a5c0ebc08b103e
hauntedguest <99336943+hauntedguest@users.noreply.github.com>
2024-06-06 03:45:19 +0530

    commit

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { useState } from 'react'
 import Hero from './Hero'
+import { useNavigate}from "react-router-dom"
 const Home = () => {
     const [value,setValue]= useState("")
+    const navigateTo = useNavigate()
+    const handleJoinMeeting = () =>{
+        navigateTo(`/room/${value}`);
+    };
   return (
    <>
     <Hero/>
@@ -15,7 +20,7 @@ const Home = () => {
                 value={value} 
                 onChange= {(e)=>setValue(e.target.value)}
             />
-            <button disabled={!value}>Join Meeting</button>
+            <button disabled={!value} onClick={handleJoinMeeting}>Join Meeting</button>
         </div>
     </div>
    </>
